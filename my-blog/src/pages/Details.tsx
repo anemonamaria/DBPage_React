bccbc78fdd38c8cc09342e8ed6fb69a85454e1b3
Anemona Maria M <anemona.maria01@gmail.com>
2022-05-16 16:53:12 +0300

    hw1 work
    Signed-off-by: Anemona Maria M <anemona.maria01@gmail.com>

---
+++ my-blog/src/pages/Details.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Details() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores dolorum quis temporibus accusamus maiores rem sequi. Illo mollitia rem reiciendis deleniti dignissimos consectetur architecto eligendi? Expedita quas cupiditate et minus aspernatur iure dolorum molestiae ipsum nihil quod, magnam atque adipisci explicabo numquam consequatur fuga magni omnis at architecto sunt minima?
+    </div>
+  );
+}
+
+export default Details;
